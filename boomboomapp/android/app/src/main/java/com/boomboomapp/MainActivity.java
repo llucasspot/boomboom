e6a066de60689bb2505d4ac9530f6efb3c39d903
llucas.spot <llucas.spot@gmail.com>
2023-12-16 02:41:11 +0100

    init: add libs

--- boomboomapp/android/app/src/main/java/com/boomboomapp/MainActivity.java
+++ boomboomapp/android/app/src/main/java/com/boomboomapp/MainActivity.java
@@ -4,8 +4,16 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-
+import android.os.Bundle;
 public class MainActivity extends ReactActivity {
+
+  /**
+   * __LIB__ : react-native-screens
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
