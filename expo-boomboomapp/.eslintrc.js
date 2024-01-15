e9391527fc97d36ce7684d9bb2bc10b238493a20
Angel-Dijoux <angel.dijoux@yahoo.com>
2024-01-15 16:41:04 +0100

    feat: add new rukes, should break CI

--- expo-boomboomapp/.eslintrc.js
+++ expo-boomboomapp/.eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
   root: true,
-  extends: ["universe/native"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "universe/native",
+    "@react-native",
+    "plugin:jest/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "import",
+    "react-hooks",
+    "react-native",
+    "unused-imports",
+  ],
+  rules: {
+    // TODO : Check how fix errors.
+    "@typescript-eslint/no-var-requires": "off",
+  },
 };
