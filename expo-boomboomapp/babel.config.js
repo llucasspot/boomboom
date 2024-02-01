23a5b5837181424373fd4215d810a52e96b30832
Johan <hoareau.johanjm@gmail.com>
2024-02-01 05:12:17 +0400

    installation reanimated

--- expo-boomboomapp/babel.config.js
+++ expo-boomboomapp/babel.config.js
@@ -1,28 +1,29 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      // Required for tsyringe
-      "babel-plugin-transform-typescript-metadata",
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      // Required for svg
-      [
-        "babel-plugin-inline-import",
-        {
-          extensions: [".svg"],
-        },
-      ],
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@swagger": "./swagger",
-            "@assets": "./src/assets",
-            "@utils": "./src/utils",
-          },
-        },
-      ],
-    ],
-  };
+module.exports = function(api) {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            // Required for tsyringe
+            "babel-plugin-transform-typescript-metadata", ["@babel/plugin-proposal-decorators", { legacy: true }],
+            // Required for svg
+            [
+                "babel-plugin-inline-import",
+                {
+                    extensions: [".svg"],
+                },
+            ],
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "@swagger": "./swagger",
+                        "@assets": "./src/assets",
+                        "@utils": "./src/utils",
+                    },
+                },
+            ],
+            // Required for reanimated
+            "react-native-reanimated/plugin",
+        ],
+    };
 };
